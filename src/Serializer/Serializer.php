d712822b45d607fc431afe4958aba15fc700a169
Christian Flothmann <christian.flothmann@xabbuh.de>
2014-03-08 13:05:27 +0100

    ease the creation of a JMS Serializer instance to be used with the xAPI

---
+++ src/Serializer/Serializer.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the XabbuhXApiCommon package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Xabbuh\XApi\Common\Serializer;
+
+use JMS\Serializer\EventDispatcher\EventDispatcher;
+use JMS\Serializer\SerializerBuilder;
+use Xabbuh\XApi\Common\Serializer\Event\ActorEventSubscriber;
+use Xabbuh\XApi\Common\Serializer\Event\ObjectEventSubscriber;
+
+/**
+ * Entry point to setup the {@link \JMS\Serializer\Serializer JMS Serializer}
+ * for the Experience API.
+ *
+ * @author Christian Flothmann <christian.flothmann@xabbuh.de>
+ */
+class Serializer
+{
+    /**
+     * Registers serialization metadata for the xAPI models on a SerializerBuilder.
+     *
+     * @param SerializerBuilder $builder The SerializerBuilder
+     */
+    public static function registerXApiMetadata(SerializerBuilder $builder)
+    {
+        $builder->addMetadataDir(__DIR__.'/../../metadata/serializer');
+    }
+
+    /**
+     * Registers event subscribers for the xAPI models on a SerializerBuilder.
+     *
+     * @param SerializerBuilder $builder The SerializerBuilder
+     */
+    public static function registerXApiEventSubscriber(SerializerBuilder $builder)
+    {
+        $builder->configureListeners(function (EventDispatcher $dispatcher) {
+            $dispatcher->addSubscriber(new ActorEventSubscriber());
+            $dispatcher->addSubscriber(new ObjectEventSubscriber());
+        });
+    }
+
+    /**
+     * Registers serialization metadata and event subscribers for the xAPI
+     * models on a SerializerBuilder.
+     *
+     * @param SerializerBuilder $builder The SerializerBuilder
+     */
+    public static function registerXApi(SerializerBuilder $builder)
+    {
+        static::registerXApiMetadata($builder);
+        static::registerXApiEventSubscriber($builder);
+    }
+
+    /**
+     * Creates a SerializerBuilder with serialization metadata and serialization
+     * event subscribers registered for the xAPI models.
+     *
+     * @return SerializerBuilder The SerializerBuilder
+     */
+    public static function createSerializerBuilder()
+    {
+        $builder = SerializerBuilder::create();
+        static::registerXApiMetadata($builder);
+        static::registerXApiEventSubscriber($builder);
+
+        return $builder;
+    }
+
+    /**
+     * Creates a new Serializer.
+     *
+     * @return \JMS\Serializer\SerializerInterface The Serializer
+     */
+    public static function createSerializer()
+    {
+        return static::createSerializerBuilder()->build();
+    }
+}
---
+++ tests/Serializer/SerializerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the XabbuhXApiCommon package.
+ *
+ * (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Xabbuh\XApi\Common\Tests\Serializer;
+
+use Xabbuh\XApi\Common\Serializer\Serializer;
+
+/**
+ * @author Christian Flothmann <christian.flothmann@xabbuh.de>
+ */
+class SerializerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRegisterXApiMetadata()
+    {
+        $serializerBuilder = $this->getMock('\JMS\Serializer\SerializerBuilder');
+        $serializerBuilder->expects($this->once())
+            ->method('addMetadataDir');
+
+        Serializer::registerXApiMetadata($serializerBuilder);
+    }
+
+    public function testRegisterXApiEventSubscriber()
+    {
+        $serializerBuilder = $this->getMock('\JMS\Serializer\SerializerBuilder');
+        $serializerBuilder->expects($this->once())
+            ->method('configureListeners');
+
+        Serializer::registerXApiEventSubscriber($serializerBuilder);
+    }
+
+    public function testRegisterXApi()
+    {
+        $serializerBuilder = $this->getMock('\JMS\Serializer\SerializerBuilder');
+        $serializerBuilder->expects($this->once())
+            ->method('addMetadataDir');
+        $serializerBuilder->expects($this->once())
+            ->method('configureListeners');
+
+        Serializer::registerXApi($serializerBuilder);
+    }
+}
